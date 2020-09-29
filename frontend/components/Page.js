d66b3a20c509b8b58cf18172371f656fbf30410c
Dorota Zelga <dorota.zelga@gmail.com>
2020-09-29 14:41:07 +0200

    meta added to oage

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import Header from './Header';
+import Meta from './Meta';
 
 class Page extends Component {
     render() {
         return (
             <div>
+                <Meta />
                 <Header />
                 <p>lol</p>
                 {this.props.children}
